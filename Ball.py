7f44e525b84f69750fb1bcd3d722ff5c94087a1f
Will <wpgrimmer@loyola.edu>
2021-11-03 20:42:48 -0400

    Finished

--- Ball.py
+++ Ball.py
@@ -1,20 +1,23 @@
 from graphicsCS151 import *
-def input_check(minimum,maximum):
-    x=input("please input a starting x coordinate from " +str(minimum) + " to " + str(maximum))
+def input_check(minimum,maximum,x):
     if not x.isdigit():
         print("please input only digits")
-        input_check(minimum,maximum)
+        return False
     x=float(x)
     if x >= minimum and x<= maximum:
-        return x
+        return True
     else:
         print("please choose a value in the given range")
-        input_check(minimum,maximum)
+        return False
 def main():
     radius=15
     minimum=radius
     maximum=500-radius
-    x_coord=input_check(minimum,maximum)
+    check=False
+    while not check:
+        x = input("please input a starting x coordinate from " + str(minimum) + " to " + str(maximum))
+        check=input_check(minimum,maximum,x)
+    x_coord=float(x)
     window = make_window("Lab 6", 500, 500)
     circle = make_circle(x_coord, 20, radius)
     color_circle(circle, "Red")
